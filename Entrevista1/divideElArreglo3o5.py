c8040ad988aa239a55a26771eb49c052c3c478f5
Samuel Rico Gómez <100243610+sricog@users.noreply.github.com>
2022-09-08 08:36:02 -0500

    Update divideElArreglo3o5.py

--- Entrevista1/divideElArreglo3o5.py
+++ Entrevista1/divideElArreglo3o5.py
@@ -9,3 +9,8 @@ def sumaGrupos(nums, i, suma5, suma3):
             return sumaGrupos(nums, i+1, suma5, suma3+nums[i])
         else:
             return sumaGrupos(nums, i+1, suma5+nums[i], suma3) or sumaGrupos(nums, i+1, suma5, suma3+nums[i])
+n = int(input())
+nums = [0]*n
+for i in range(n):
+	nums[i] = int(input())
+print(sumaGrupos(nums,0,0,0))
