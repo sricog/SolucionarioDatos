7f943dbb38c48e4133ec1b6f38f2067060c34079
Samuel Rico Gómez <100243610+sricog@users.noreply.github.com>
2022-09-08 08:35:30 -0500

    Create divideElArreglo3o5.py

---
+++ Entrevista1/divideElArreglo3o5.py
@@ -0,0 +1,11 @@
+#Divide el arreglo con multiplos de 3 o 5 
+def sumaGrupos(nums, i, suma5, suma3):
+    if i == len(nums):
+        return suma5 == suma3
+    else:
+        if nums[i]%5 == 0:
+            return sumaGrupos(nums, i+1, suma5+nums[i], suma3)
+        elif nums[i]%3 == 0:
+            return sumaGrupos(nums, i+1, suma5, suma3+nums[i])
+        else:
+            return sumaGrupos(nums, i+1, suma5+nums[i], suma3) or sumaGrupos(nums, i+1, suma5, suma3+nums[i])
